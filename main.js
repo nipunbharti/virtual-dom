42b805cadfda7e6c0c1af7beefe11a5d2c01cdaf
nipunbharti <ced16i026@iiitdm.ac.in>
2018-12-07 16:24:27 +0530

    Added: Support for text elements

--- main.js
+++ main.js
@@ -2,11 +2,15 @@ import createElement from './vdom/createElement';
 import render from './vdom/render';
 import mount from './vdom/mount';
 
-const vApp = createElement('div', {
+let count = 0;
+let text = 'Hello';
+const createVApp = (count) => createElement('div', {
 	attrs: {
-		id: 'app'
+		id: 'app',
+		dataCount: count
 	},
 	children: [
+		String(text),
 		createElement('img', {
 			attrs: {
 				src: 'https://media.giphy.com/media/bHELdJXl1LDG0/giphy.gif'
@@ -15,6 +19,7 @@ const vApp = createElement('div', {
 	]
 });
 
+const vApp = createVApp(count);
 const $app = render(vApp);
 mount($app, document.getElementById('app'));
 
--- vdom/render.js
+++ vdom/render.js
@@ -1,18 +1,26 @@
-const render = (vNode) => {
-    const $el = document.createElement(vNode.tagName);
+const renderElem = ({tagName, attrs, children}) => {
+    const $el = document.createElement(tagName);
 
     // Set Attributes
-    for (const [k, v] of Object.entries(vNode.attrs)) {
+    for (const [k, v] of Object.entries(attrs)) {
         $el.setAttribute(k ,v);
     }
 
     // Set Children recursively
-    for (const child of vNode.children) {
+    for (const child of children) {
         const $child = render(child);
         $el.appendChild($child);
     }
 
     return $el;
+}
+
+const render = (vNode) => {
+    if(typeof vNode === 'string') {
+        return document.createTextNode(vNode);
+    }
+
+    return renderElem(vNode);
 }
 
 export default render;
